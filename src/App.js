e0294c03921ac420f014d82f80dd61e3449fd965
Lbb <wojiaolianbb@163.com>
2021-11-11 22:51:32 +0800

    feat:路由定义

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Route, Navigate, Routes } from "react-router-dom";
+import React from "react";
+import routes from "./route";
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header>头部</header>
+        <Routes>
+          {routes.map((route) => {
+            return (
+              <Route
+                key={route.path}
+                path={route.path}
+                component={route.component}
+              />
+            );
+          })}
+          <Navigate exact from="/" to={routes[0].path} />
+          {/* 这里用 Navigate  进行 首页自动跳转到 /home 路由下 
+                 exact 意味着精确匹配 当为 / 时才跳转 /home 不是包含 / 就跳转到 /home
+             */}
+          <Navigate to="/404" />
+          {/* 如果找不到页面 则去 4040页面 */}
+        </Routes>
+        <footer>尾部</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import routes from "./route";
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router>
+    <Routes>
+      {routes.map((route) => {
+        return (
+          <Route
+            key={route.path}
+            path={route.path}
+            element={route.component}
+          />
+        );
+      })}{" "}
+    </Routes>
+  </Router>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
