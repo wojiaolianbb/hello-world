27dc9f66a4bd51d8e11bfccdbd0fac77c5b2ca86
lianjf <lianjianfeng@dxy.cn>
2021-12-31 16:25:07 +0800

    faet: 添加视频源

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React,{useEffect} from "react";
 import ProtoTypes from "prop-types";
 import classNames from 'classnames/bind';
 import styles from './index.less';
+
 let cx = classNames.bind(styles);
+const handleVideoInit = (url) => {
+  new window.Aliplayer({
+    "id": "player-con",
+    "source": url,
+    "width": "1200px",
+    'height': '100vh',
+    "autoplay": true,
+    "isLive": true,
+    "rePlay": false,
+    "playsinline": true,
+    "preload": true,
+    "controlBarVisibility": "hover",
+    "useH5Prism": true
+  }, function (player) {
+      console.log("The player is created");
+    }
+  );
+};
 
 const Home = () => {
+  const url =  "http://1011.hlsplay.aodianyun.com/demo/game.flv";
+  useEffect(() => handleVideoInit(url), []);
   return (
     <div>
       <h2 className={cx('home')}>
-          当前是 Home 页面，可以选择跳转到 Setting 界面或 Component 界面
+          <div className="prism-player" id="player-con"></div>
+
       </h2>
     </div>
   );
